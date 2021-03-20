b7e24591beb002184121efa68def942488dbf6cb
bepyan <rlantia97@kumoh.ac.kr>
2021-03-20 23:45:20 +0900

    Singleton Test

--- src/test/java/com/kit/dorm/IocContainerTest/IocContainerTest.java
+++ src/test/java/com/kit/dorm/IocContainerTest/IocContainerTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import org.assertj.core.api.Assert;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class IocContainerTest {
     @Test
     void findApplicationBean() {
@@ -14,6 +17,16 @@ public class IocContainerTest {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
             System.out.println(beanDefinitionName+"___"+beanDefinition);
         }
+    }
+
+    @Test
+    void SingletonTest(){
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+
+        System.out.println("instance1 = " + instance1);
+        System.out.println("instance2 = " + instance2);
+        assertThat(instance1).isSameAs(instance2);
     }
 
 }
---
+++ src/test/java/com/kit/dorm/IocContainerTest/Singleton.java
@@ -0,0 +1,15 @@
+package com.kit.dorm.IocContainerTest;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if(instance == null)
+            return instance = new Singleton();
+
+        return instance;
+    }
+}
