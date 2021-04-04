38a619e3682aad8e885ae9754651e9991d0e7a22
bepyan <rlantia97@kumoh.ac.kr>
2021-04-04 23:41:11 +0900

    class 4-2 Autowired, Lombok

--- src/main/java/com/kit/dorm/member/MemberServiceImpl.java
+++ src/main/java/com/kit/dorm/member/MemberServiceImpl.java
@@ -1,20 +1,17 @@
 package com.kit.dorm.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
     /* FileMemberStorage가 아닌 interface를 가져오는게 국룰 */
     // private final MemberStorage memberStorage = new DbMemberStorage();
     // private final MemberStorage memberStorage = new FileMemberStorage();
     /* 코드 수정 사항이 불가피한 단점이 존재(OCP 준수 X) => AppConfig로 해결(의존성 주입) */
-    private MemberStorage memberStorage;
-
-    @Autowired
-    public MemberServiceImpl(MemberStorage memberStorage) {
-        this.memberStorage = memberStorage;
-    }
+    private final MemberStorage memberStorage;
 
     @Override
     public void register(Member member) {
