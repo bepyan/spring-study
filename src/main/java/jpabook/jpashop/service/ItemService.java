81062131c399c0bf5036bae25774df99a1d97bb4
bepyan <rlantia97@kumoh.ac.kr>
2021-05-23 00:56:12 +0900

    11주차 Item 서비스

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -4,16 +4,17 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
 
+    @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
     }
